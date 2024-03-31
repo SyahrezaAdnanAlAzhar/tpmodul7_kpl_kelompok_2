e3e245be780411309065a963e5987dbfb3acce47
Ricky Renaldi <rickyrenaldi100@gmail.com>
2024-04-01 05:02:00 +0700

    menambahkan class DataMahasiswa1302223051

--- tpmodul7_kpl_kelompok_2/program_tp_mod_7_kpl/Program.cs
+++ tpmodul7_kpl_kelompok_2/program_tp_mod_7_kpl/Program.cs
@@ -2,8 +2,8 @@
 {
     public static void Main(string[] args)
     {
-        DataMahasiswa1302223041 mahasiswa1302223041 = new DataMahasiswa1302223041();
+        DataMahasiswa1302223051 mahasiswa1302223051 = new DataMahasiswa1302223051();
 
-        mahasiswa1302223041.ReadJSON();
+        mahasiswa1302223051.ReadJSON();
     }
 }
