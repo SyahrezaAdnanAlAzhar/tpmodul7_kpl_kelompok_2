cd9623f654d29577cccce3414b5427c375877073
Syahreza Adnan Al Azhar <notsupersyahreza@student.telkomuniversity.ac.id>
2024-04-01 04:55:52 +0700

    Menambahkan Class KuliahMahasiswa

--- tpmodul7_kpl_kelompok_2/program_tp_mod_7_kpl/Program.cs
+++ tpmodul7_kpl_kelompok_2/program_tp_mod_7_kpl/Program.cs
@@ -19,5 +19,8 @@
 
         DataMahasiswa1302223051 mahasiswa1302223051 = new DataMahasiswa1302223051();
         mahasiswa1302223051.ReadJSON();
+
+        KuliahMahasiswa1302223041 kuliah1302223041 = new KuliahMahasiswa1302223041();
+        kuliah1302223041.ReadJSON();
     }
 }
